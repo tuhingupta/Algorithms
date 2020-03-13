adf79085ab5e118244ed41a1426e08de854ac4e9
Tuhin Gupta <tuhin.gupta@gmail.com>
2020-03-13 17:56:51 -0500

    Update SpacedPermutation.java

--- src/com/other/problems/SpacedPermutation.java
+++ src/com/other/problems/SpacedPermutation.java
@@ -33,28 +33,21 @@ public class SpacedPermutation {
 	
 	public static void main(String[] args) {
 		
-		int input = 3;	
+		//int input = 3;	
+		//=[2, 3, 1, 2, 1, 3]
+		//=[3, 1, 2, 1, 3, 2]
+		
+		int input = 4;
+		//=[4, 1, 3, 1, 2, 4, 3, 2]
 		
 		int[] ary = new int[input*2];
-		
+		int[] wip = new int[input*2];
 		
 		int i = 1;
 		createAry(ary, i, input *2);
-		
-		int[] wip = new int[input*2];
-		
-		//initialize empty array to set values
-		
 
 		//initialize empty array for only input length
 		LinkedList<Integer> list ;
-
-//		for(int k=0; k< input; k++)
-//		{
-//			list.add(k+1);
-//		}
-		
-		//Iterator<Integer> itr = list.iterator();
 		
 		for(int j=1;j<= input; j++){
 		
@@ -68,47 +61,34 @@ public class SpacedPermutation {
 			}
 			
 			System.out.println("recursion "+list.toString()+" "+Arrays.toString(wip)+" "+j);
-			recursive(list, wip, j);
+			if(recursive(list, wip, j))
+				System.out.println("="+Arrays.toString(wip));
 		}
-		
-		
 		
 	}
 	
-	public static void recursive(LinkedList<Integer> list, int[] wip, int j){
+	public static boolean recursive(LinkedList<Integer> list, int[] wip, int j){
 		
-		//System.out.println(Arrays.toString(wip));
 		
 		list.remove(new Integer(j));
-		boolean bool = false;
+		
 		
 		for(int i = 0; i < wip.length; i++){
 			
 			if(wip[i] == 0 && i+j+1 < wip.length && wip[i+j+1] == 0){
 				wip[i] = j;
 				wip[i+j+1] = j;
-				bool = true;
-				break;
-			}else continue;
+				if(!list.isEmpty())
+					return true && recursive(list, wip, list.getFirst());
+				else 
+					return true;
+			}
+			
 		}//for
 		
-		if(!bool) return;
-		
-		while(!list.isEmpty()){
-			Integer t = list.getFirst();
-			recursive(list, wip, t);
-		}
-		
-		if(bool && list.isEmpty())
-			System.out.println(Arrays.toString(wip));
+		return false;
 
-		
-		return;
-		
-		
 	}
-	
-	
 	
 	public static void createAry(int[] ary, int i, int limit){
 		
